46ac2d1a698a174a3679e983c735a204ee1205cc
zhoujunwei <zjw505104341@163.com>
2021-12-03 11:42:05 +0800

    test

---
+++ setup.py
@@ -0,0 +1,17 @@
+# !/usr/bin/python3
+# -*-coding:utf-8-*-
+# Author: zhou jun wei
+# CreatDate: 2021/12/3 11:38
+
+from setuptools import find_packages, setup
+setup(
+    name='zjwocr',  #　包的名字
+    version='0.0.1',
+    description='test',
+    author='zhoujunwei',
+    author_email='505104341@qq.com',
+    url='https://github.com/zjw505104341/ocrimg',
+    #packages=find_packages(),
+    packages=['autotest'],
+    #install_requires=['requests'],
+)
