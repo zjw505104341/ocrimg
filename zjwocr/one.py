0f49ec3f80060f671373ef418811a04d55e8d2df
zhoujunwei <zjw505104341@163.com>
2021-12-03 11:41:44 +0800

    test

---
+++ zjwocr/one.py
@@ -0,0 +1,10 @@
+# !/usr/bin/python3
+# -*-coding:utf-8-*-
+# Author: zhou jun wei
+# CreatDate: 2021/12/3 11:40
+import os, sys, datetime, platform, traceback, time, uuid, hashlib, math, re
+
+def add(a, b):
+    return a+b
+
+
